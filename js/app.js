b2a36b8e9483d2f90de253d8e2ad93f993abbb0b
Maggie Dalke <cdalke4@msn.com>
2020-12-14 20:08:16 -0600

    corrected script link, added function with nestedcallbacks.

---
+++ js/app.js
@@ -0,0 +1,92 @@
+const makeCake2 = function () {
+  setTimeout(() => {
+    console.log('Step 1: Preheat oven to 175 degrees C.');
+    setTimeout(() => {
+      console.log('Step 2: Grease and flour a 9x9 inch pan.');
+      setTimeout(() => {
+        console.log(
+          'Step 3: In a medium bowl, cream together the sugar and butter.'
+        );
+        setTimeout(() => {
+          console.log('Step 4: Beat in the eggs, one at a time.');
+          setTimeout(() => {
+            console.log('Step 5: Stir in the vanilla.');
+            setTimeout(() => {
+              console.log('Step 6: Combine flour and baking powder');
+              setTimeout(() => {
+                console.log(
+                  'Step 7: Add dry ingredients to the creamed mixture and mix well.'
+                );
+                setTimeout(() => {
+                  console.log(
+                    'Step 8: Stir in the milk until batter is smooth.'
+                  );
+                  setTimeout(() => {
+                    console.log(
+                      'Step 9: Pour or spoon batter into the prepared pan.'
+                    );
+                    setTimeout(() => {
+                      console.log('Step 10: Bake for 30 to 40 minutes.');
+                      setTimeout(() => {
+                        console.log(
+                          'Step 11: Cake is done when it springs back to the touch.'
+                        );
+                      }, 1);
+                    }, 3000);
+                  }, 6000);
+                }, 4000);
+              }, 300);
+            }, 2000);
+          }, 1000);
+        }, 5000);
+      }, 3000);
+    }, 200);
+  }, 20);
+};
+
+makeCake2();
+
+/* 
+ORIGINAL FUNCTION
+*****************
+const makeCake = function () {
+  setTimeout(() => {
+    console.log('Step 1: Preheat oven to 175 degrees C.');
+  }, 20);
+  setTimeout(() => {
+    console.log('Step 2: Grease and flour a 9x9 inch pan.');
+  }, 200);
+  setTimeout(() => {
+    console.log(
+      'Step 3: In a medium bowl, cream together the sugar and butter.'
+    );
+  }, 3000);
+
+  setTimeout(() => {
+    console.log('Step 4: Beat in the eggs, one at a time.');
+  }, 5000);
+  setTimeout(() => {
+    console.log('Step 5: Stir in the vanilla.');
+  }, 1000);
+  setTimeout(() => {
+    console.log('Step 6: Combine flour and baking powder');
+  }, 2000);
+  setTimeout(() => {
+    console.log(
+      'Step 7: Add dry ingredients to the creamed mixture and mix well.'
+    );
+  }, 300);
+  setTimeout(() => {
+    console.log('Step 8: Stir in the milk until batter is smooth.');
+  }, 4000);
+  setTimeout(() => {
+    console.log('Step 9: Pour or spoon batter into the prepared pan.');
+  }, 6000);
+  setTimeout(() => {
+    console.log('Step 10: Bake for 30 to 40 minutes.');
+  }, 3000);
+  setTimeout(() => {
+    console.log('Step 11: Cake is done when it springs back to the touch.');
+  }, 1);
+};
+ */
